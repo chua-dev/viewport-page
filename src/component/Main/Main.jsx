48ac5bef18a35d93f10b562c9bb54105fa612641
dev-chua <chua3940@gmail.com>
2022-06-15 23:21:12 +0800

    added background, with react icon, dynamic font size

--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
 import { useEffect, useState } from 'react'
-
+import './Main.css'
+import { BsStack } from 'react-icons/bs'
+import {TbViewportWide} from 'react-icons/tb'
+import {CgScreenShot} from 'react-icons/cg'
 
 const Main = () => {
 
@@ -23,16 +26,17 @@ const Main = () => {
     const sh = window.screen.height
 
     document.getElementById('viewport-main').innerHTML = `${vw}px (W) X ${vh}px (H)`
-    document.getElementById('screen-size').innerHTML = `Full Screen: ${sw}px (W) X ${sh}px (H)`
+    document.getElementById('screen-size').innerHTML = `Full Screen: ${sw}px x ${sh}px`
 
     window.addEventListener('resize', handleResize)
 
   },)
 
   return (
-    <Container>
+    <Container className='container'>
       <TextContainer>
         <FlexContainer>
+          <CgScreenShot size={60}/>
           <Title>Your device viewport size is:</Title>
           <ViewSize id='viewport-main'></ViewSize>
           <Line></Line>
@@ -43,7 +47,9 @@ const Main = () => {
   )
 }
 
-const Container = styled.div``;
+const Container = styled.div`  
+  
+`;
 const TextContainer = styled.div`
   display: flex;
   justify-content: center;
@@ -52,15 +58,19 @@ const TextContainer = styled.div`
   min-height: 90vh;
 `;
 const Title = styled.h2`
-  font-size: 2.3em;
+  font-size: calc(100% + 1.5vw);
+  
 `;
 const ViewSize = styled.h1`
-  font-size: 3em;
-  color: blue;
+  font-size: calc(100% + 3.9vw);
+  font-weight: bolder;
+  //text-shadow: 1px 2.5px #bcb8b8;
 `;
 
 const FlexContainer = styled.div``;
-const ScreenSize = styled.h2``;
+const ScreenSize = styled.h2`
+  font-size: calc(100% + 1.1vw);
+`;
 const Line = styled.hr``;
 
 //document.getElementById('viewport-main').innerHTML = "Your Device View Port is Height"
